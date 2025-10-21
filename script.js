69489afacf25c81ff0605c6d56c31627596f3027
ryujin49 <muhammadazizviib@gmail.com>
2025-10-21 08:30:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-//1. Pilih tombol berdasarkan ID-nya 
 const tombolsapa = document.getElemenByid('sapaBotton');
-//2.Tambahkan 'event listener' untuk aksi 'click'
 tombolsapa.addEventlistener('click', function() {
-    //3. jalankan kode ini ketika tombol di-klik
-    alert('halo! Terima kasih suda berkunjung!');
+  alert('halo! Terima kasih suda berkunjung!');
 });
